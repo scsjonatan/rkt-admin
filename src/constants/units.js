da0fcc885bd20c8a3b2dd0dd670e15a499ac7dfb
Jonatan <jonatanscs97@gmail.com>
2018-11-18 10:25:41 -0600

    Complete select fill

---
+++ src/constants/units.js
@@ -0,0 +1,41 @@
+export const TYPES = [
+  {
+    value: 1,
+    label: 'Departamentos'
+  },
+  {
+    value: 2,
+    label: 'Casas'
+  },
+  {
+    value: 3,
+    label: 'Terrenos'
+  },
+  {
+    value: 4,
+    label: 'Oficinas/Locales'
+  },
+  {
+    value: 5,
+    label: 'Bodegas'
+  },
+  {
+    value: 6,
+    label: 'Otros'
+  }
+]
+
+export const STATUS = [
+  {
+    value: 'active',
+    label: 'Disponible'
+  },
+  {
+    value: 'hidden',
+    label: 'Vendido'
+  },
+  {
+    value: 'deleted',
+    label: 'Eliminado'
+  }
+]
